95d0d3bb4864adc0745ea81eec3874c26f1442b4
Alexander Velikorechin <alexander.a.velikore@megafon.ru>
2021-01-30 14:12:07 +0300

    Module 3: prop types checking

--- module_3_jotto-app/src/Congrats.test.js
+++ module_3_jotto-app/src/Congrats.test.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-const setup = (props = {}) => shallow(<Congrats {...props} />);
+const defaultProps = { success: false };
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
+};
 
 test("renders without error", () => {
   const wrapper = setup();
@@ -16,7 +20,7 @@ test("renders without error", () => {
 });
 
 test("renders no text when `success` prop is false", () => {
-  const wrapper = setup({ success: false });
+  const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.text()).toBe("");
 });
@@ -26,3 +30,10 @@ test("renders non-empty success message when `success` prop is true", () => {
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
 });
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
+});
+
+test("does throw warning with wrong props", () => {});
