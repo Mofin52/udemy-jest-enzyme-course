feda0365f246aab0761b9dda5d6b21143ca6773e
Alexander Velikorechin <alexander.a.velikore@megafon.ru>
2021-02-02 12:39:18 +0300

    Module 5: init

---
+++ modules_3_4_jotto-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import GuessedWords from "./GuessedWords";
+import Congrats from "./Congrats";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[
+            {
+              guessedWord: "train",
+              letterMatchCount: 3,
+            },
+          ]}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
